57562385a049b9a13a714efa315a5457007db0b7
Eragon peerGynt <eragon.peergynt@gmail.com>
2016-05-22 09:56:08 +0000

    konec dela na beleženju podatkov. Podatki za litre in minute shranjeni v tlakugit add *!

---
+++ knjiznice/js/pomoc.js
@@ -0,0 +1,215 @@
+
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
+
+var username = "ois.seminar";
+var password = "ois4fri";
+
+
+/**
+ * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
+ * enolične ID številke za dostop do funkcionalnosti
+ * @return enolični identifikator seje za dostop do funkcionalnosti
+ */
+function getSessionId() {
+    var response = $.ajax({
+        type: "POST",
+        url: baseUrl + "/session?username=" + encodeURIComponent(username) +
+                "&password=" + encodeURIComponent(password),
+        async: false
+    });
+    return response.responseJSON.sessionId;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function kreirajUporabnikID() {
+	sessionId = getSessionId();
+
+	var ime = $("#trenutnoIme").val();
+	var priimek = $("#trenutniPriimek").val();
+	var datumRojstva = $("#datumRojstva").val();
+
+	if (!ime || !priimek || !datumRojstva || ime.trim().length == 0 ||
+      priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
+		$("#kreirajSporocilo").html("<span class='obvestilo label " +
+      "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#preberiEHRid").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		    }
+		});
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function preglejUporabnika() {
+	sessionId = getSessionId();
+
+	var ehrId = $("#preberiUporabnika").val();
+
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#preberiSporocilo").html("<span class='obvestilo label label-warning " +
+      "fade-in'>Prosim vnesite zahtevan podatek!");
+	} else {
+		$.ajax({
+			url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+			type: 'GET',
+			headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+				var party = data.party;
+				$("#preberiSporocilo").html("<span class='obvestilo label " +
+          "label-success fade-in'>Uporabnik '" + party.firstNames + " " +
+          party.lastNames + "', ki se je rodil '" + party.dateOfBirth +
+          "'.</span>");
+			},
+			error: function(err) {
+				$("#preberiSporocilo").html("<span class='obvestilo label " +
+          "label-danger fade-in'>Napaka '" +
+          JSON.parse(err.responseText).userMessage + "'!");
+			}
+		});
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function dodajMeritve() {
+	sessionId = getSessionId();
+
+	var ehrId = $("#dodajAktivnostEhrID").val();
+	var datumInUra = $("#zabeleziUro").val();
+	var telesnaVisina = $("#dodajVisina").val();
+	var telesnaTeza = $("#dodajTeza").val();
+	var trajanjeTelovadbe = $("#dodajMinAktivnosti").val();//var sistolicniKrvniTlak = $("#dodajVitalnoKrvniTlakSistolicni").val();
+	var zauzitaVoda = $("#dodajVoda").val();//var diastolicniKrvniTlak = $("#dodajVitalnoKrvniTlakDiastolicni").val();
+	var merilec = "uporabnik";
+
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#dodajMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo " +
+      "label label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		var podatki = {
+			// Struktura predloge je na voljo na naslednjem spletnem naslovu:
+      // https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+		    "ctx/language": "en",
+		    "ctx/territory": "SI",
+		    "ctx/time": datumInUra,
+		    "vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+		    "vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+		    "vital_signs/blood_pressure/any_event/systolic": trajanjeTelovadbe,
+		    "vital_signs/blood_pressure/any_event/diastolic": zauzitaVoda,
+		};
+		var parametriZahteve = {
+		    ehrId: ehrId,
+		    templateId: 'Vital Signs',
+		    format: 'FLAT',
+		    committer: merilec
+		};
+		$.ajax({
+		    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+		    type: 'POST',
+		    contentType: 'application/json',
+		    data: JSON.stringify(podatki),
+		    success: function (res) {
+		        $("#dodajMeritveVitalnihZnakovSporocilo").html(
+              "<span class='obvestilo label label-success fade-in'>" +
+              res.meta.href + ".</span>");
+		    },
+		    error: function(err) {
+		    	$("#dodajMeritveVitalnihZnakovSporocilo").html(
+            "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+            JSON.parse(err.responseText).userMessage + "'!");
+		    }
+		});
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+
+  //prepripravljeni uporabniki
+  
+    $('#preberiPredlogoBolnika').change(function() {
+        $("#kreirajSporocilo").html("");
+        var podatki = $(this).val().split(",");
+        $("#trenutnoIme").val(podatki[0]);
+        $("#trenutniPriimek").val(podatki[1]);
+        $("#datumRojstva").val(podatki[2]);
+    });
+  
+    $('#preberiObstojeciEHR').change(function() {
+		$("#preberiSporocilo").html("");
+		$("#preberiUporabnika").val($(this).val());
+    });
+    
+    $('#preberiObstojeciVitalniZnak').change(function() {
+		$("#dodajMeritveVitalnihZnakovSporocilo").html("");
+		var podatki = $(this).val().split("|");
+		$("#dodajAktivnostEhrID").val(podatki[0]);
+		$("#zabeleziUro").val(podatki[1]);
+		$("#dodajVisina").val(podatki[2]);
+		$("#dodajTeza").val(podatki[3]);
+		$("#dodajMinAktivnosti").val(podatki[4]);
+		$("#dodajVoda").val(podatki[5]);
+	});
+  
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Generator podatkov za novega pacienta, ki bo uporabljal aplikacijo. Pri
+ * generiranju podatkov je potrebno najprej kreirati novega pacienta z
+ * določenimi osebnimi podatki (ime, priimek in datum rojstva) ter za njega
+ * shraniti nekaj podatkov o vitalnih znakih.
+ * @param stPacienta zaporedna številka pacienta (1, 2 ali 3)
+ * @return ehrId generiranega pacienta
+ */
+function generirajPodatke(stPacienta) {
+  ehrId = "";
+
+  // TODO: Potrebno implementirati
+
+  return ehrId;
+}
+
+
+// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
